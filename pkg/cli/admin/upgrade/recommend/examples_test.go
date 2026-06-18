65611b9f7b64b67dcc16bf7aac0c7b6b24711488
Nick Bottari <nbottari9@gmail.com>
2026-06-18 14:07:10 -0400

    fix(adm-upgrade-recommend-alerts): added new mockData test case to test ignoring alert checking
    Signed-off-by: Nick Bottari <nbottari9@gmail.com>

--- pkg/cli/admin/upgrade/recommend/examples_test.go
+++ pkg/cli/admin/upgrade/recommend/examples_test.go
@@ -70,6 +70,7 @@ func TestExamples(t *testing.T) {
 				"examples/4.19.0-okd-scos.16-cv.yaml":                       "4.19.0-okd-scos.17",
 				"examples/4.22.0-extend-recommended-alert-cv.yaml":          "1.2.3-not-important",
 				"examples/4.22.0-extend-recommended-critical-alert-cv.yaml": "1.2.3-not-important",
+				"examples/5.0.0-cvo-handling-risks-cv.yaml":                 "1.2.3-not-important",
 			},
 			outputSuffixPattern: ".version-%s-output",
 		},
